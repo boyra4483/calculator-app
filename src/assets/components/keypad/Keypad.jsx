3eb1b47117751b678f63a6c9d145cf50188e346b
boyra <b9892213@gmail.com>
2025-02-06 15:23:39 +0300

    seventh

--- src/assets/components/keypad/Keypad.jsx
+++ src/assets/components/keypad/Keypad.jsx
@@ -4,21 +4,14 @@ import { operators } from "../../common/common";
 
 export default function Keypad({ onClick, expression }) {
   function handleClick(e) {
-    const target = e.target;
     const key = e.target.textContent;
     const splitedExpression = getSplitExpression(
       key == "=" ? expression : expression + key
     );
 
-    if (operators.includes(key) && isOperatorDuplicated(expression + key))
-      return;
-    if (target.tagName != "DIV") return;
+    if (!isValidData(expression, key, e.target)) return;
     if (key == "AC") return onClick("");
 
-    // the first keys must not be operators
-    if (!expression && operators.includes(key)) return;
-
-    if ((expression + key).endsWith("=") && !splitedExpression) return;
     if (splitedExpression && key == "=") {
       return onClick(calculating(splitedExpression));
     }
@@ -26,30 +19,13 @@ export default function Keypad({ onClick, expression }) {
       return onClick(
         `${expression.startsWith("-") ? expression.slice(1) : `-${expression}`}`
       );
+
     onClick(key == "+/-" ? expression : expression + key);
   }
 
   return (
     <section className={classes["keypad"]} onClick={handleClick}>
-      <Button value="AC" />
-      <Button value="+/-" />
-      <Button value="%" />
-      <Button value="/" />
-      <Button value="7" />
-      <Button value="8" />
-      <Button value="9" />
-      <Button value="X" />
-      <Button value="4" />
-      <Button value="5" />
-      <Button value="6" />
-      <Button value="-" />
-      <Button value="1" />
-      <Button value="2" />
-      <Button value="3" />
-      <Button value="+" />
-      <Button value="0" />
-      <Button value="." />
-      <Button value="=" />
+      {getKeyButtons()}
     </section>
   );
 }
@@ -63,7 +39,6 @@ function calculating([x, operator, y]) {
     case "-":
       return hasRemainder ? (x - y).toFixed(2) : x - y;
     case "X":
-      console.log(hasRemainder ? (x * y).toFixed(2) : x * y);
       return hasRemainder ? (x * y).toFixed(2) : x * y;
     case "/":
       return x % y != 0 ? (x / y).toFixed(2) : x / y;
@@ -99,4 +74,55 @@ function isOperatorDuplicated(expression) {
       )[0]
     ) || operators.find((operator) => copyExpression.includes(operator))
   );
+}
+
+function getKeyButtons() {
+  return (
+    <>
+      <Button value="AC" />
+      <Button value="+/-" />
+      <Button value="%" />
+      <Button value="/" />
+      <Button value="7" />
+      <Button value="8" />
+      <Button value="9" />
+      <Button value="X" />
+      <Button value="4" />
+      <Button value="5" />
+      <Button value="6" />
+      <Button value="-" />
+      <Button value="1" />
+      <Button value="2" />
+      <Button value="3" />
+      <Button value="+" />
+      <Button value="0" />
+      <Button value="." />
+      <Button value="=" />
+    </>
+  );
+}
+
+function isValidData(expression, key, target) {
+  const splitedExpression = getSplitExpression(
+    key == "=" ? expression : expression + key
+  );
+
+  if (target.tagName != "DIV") return false;
+  if (!expression && operators.includes(key)) return false;
+  if (operators.includes(key) && isOperatorDuplicated(expression + key))
+    return false;
+  if ((expression + key).endsWith("=") && !splitedExpression) return false;
+  if (key == "." && !splitedExpression && expression.includes(key))
+    return false;
+
+  if (key == "." && splitedExpression) {
+    const exp = splitedExpression[2].split("");
+    exp.splice(
+      splitedExpression.slice().findIndex((char) => char == "."),
+      1
+    );
+    return exp.includes(".") ? false : true;
+  }
+  if (!splitedExpression && key == "%") return false;
+  return true;
 }
